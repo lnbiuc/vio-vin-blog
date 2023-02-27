0827ee8bbd57f2c276420ceaa423180620a24a3b
ayanamirei <violetzzs@proton.me>
2023-02-27 20:12:38 +0800

    :tada:

---
+++ components/navs/Footer.vue
@@ -0,0 +1,9 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+    <div class="h-10 bg-gray">
+        Copyright 2023 by <a href="https://www.linkedin.com/in/andrew-lee-5b1b2b1b3/" target="_blank">Andrew Lee</a>. All rights reserved.
+    </div>
+</template>
